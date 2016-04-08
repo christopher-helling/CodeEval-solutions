97e20f50e8a3b879fdb86fa3354fa4b84a6cf72e
christopher-helling <christopher_helling@yahoo.com>
2016-04-07 23:53:21 -0400

    Create decimal-to-binary.cs

---
+++ Medium-problems/decimal-to-binary.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+
+namespace CodeEval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = File.OpenText(args[0]))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    if (String.IsNullOrEmpty(line))
+                        continue;
+                    // do something with line
+
+                    // maximum subarray problem
+                    string[] input = line.Split(new char[] { ',' }, StringSplitOptions.None); // split on comma
+
+                    int decimalValue = Int32.Parse(input[0]); // convert to int 
+
+                    // print result 
+                    Console.WriteLine(Convert.ToString(decimalValue, 2)); // convert decimal value to base 2
+                }
+
+            }
+
+        }
+    }
+}
