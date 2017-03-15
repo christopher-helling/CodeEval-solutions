c290433b903f62b8db09dd85548353f955bb8ca0
christopher-helling <christopher_helling@yahoo.com>
2017-03-15 03:31:36 -0400

    Create decimal-to-binary.java

---
+++ Medium-problems/decimal-to-binary.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+    public static void main (String[] args) throws IOException {
+        File file = new File(args[0]);
+        BufferedReader buffer = new BufferedReader(new FileReader(file));
+        String line;
+
+        while ((line = buffer.readLine()) != null) {
+            line = line.trim();
+            // Process line of input Here
+
+            System.out.println(Integer.toBinaryString(Integer.parseInt(line)));
+            
+        }
+        
+        buffer.close();
+    }
+}
