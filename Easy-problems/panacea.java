88e99d67f5ed13885efef91add132803d3e45c4c
christopher-helling <christopher_helling@yahoo.com>
2017-03-19 04:35:17 -0400

    Replace Java 8 streams
    Java 8 streams are neat, but they aren't as fast as the old fashioned way

--- Easy-problems/panacea.java
+++ Easy-problems/panacea.java
@@ -10,18 +10,34 @@ public class Main {
         while ((line = buffer.readLine()) != null) {
             line = line.trim();
             // Process line of input Here
-
             String[] samples = line.split("\\|"); // must delimit pipe to split in regex
-            int hexValues = Arrays.asList(samples[0].trim().split(" ")).stream().mapToInt(s -> Integer.parseInt(s, 16)).sum();
-            int binValues = Arrays.asList(samples[1].trim().split(" ")).stream().mapToInt(s -> Integer.parseInt(s, 2)).sum();
+            String[] hexValuesStr = samples[0].trim().split(" ");
+            String[] binValuesStr = samples[1].trim().split(" ");
             
+            int[] hexArray = new int[hexValuesStr.length];
+            for (int i=0; i < hexArray.length; i++) {
+            	hexArray[i] = Integer.parseInt(hexValuesStr[i], 16);
+            }
+            int[] binArray = new int[binValuesStr.length];
+            for (int i=0; i < binArray.length; i++) {
+            	binArray[i] = Integer.parseInt(binValuesStr[i], 2);
+            }
+            
+            int hexValues = 0;
+            for (int i = 0; i < hexArray.length; i++)
+            {
+            	hexValues += hexArray[i];
+            }
+            int binValues = 0;
+            for (int i = 0; i < binArray.length; i++)
+            {
+            	binValues += binArray[i];
+            }
+             
             System.out.println(binValues >= hexValues ? "True" : "False");
             
         }
         
         buffer.close();
-    }
-    
-    
-    
+    }   
 }
